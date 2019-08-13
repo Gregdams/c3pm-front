e5434fe872a5cbfb70fe701c96241ef746665d55
Geographer <geographer@live.fr>
2019-08-13 19:58:25 +0200

    Switch to styled component

--- components/ModuleCard/ModuleCard.tsx
+++ components/ModuleCard/ModuleCard.tsx
@@ -10,12 +10,16 @@ const Box = styled.div`
   box-shadow: 0 1px #FFFFFF inset, 0 1px 3px rgba(34, 25, 25, 0.4);
 `;
 
+const BigTitle = styled.p`
+  font-size: 14px;
+`;
+
 const ModuleCard = ({ module }: Props): JSX.Element => (
   <Box className="p2 bg-white">
     <p className="h3 bold m0">{ module.name }</p>
-    <p className="italic m0 mt1" style={{ fontSize: '14px' }}>
+    <BigTitle className="italic m0 mt1">
       Authored by <a href="/">{ module.author }</a>
-    </p>
+    </BigTitle>
 
     <p className="m0 mt3">{ module.description }</p>
   </Box>
